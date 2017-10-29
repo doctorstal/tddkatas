a0658844cd2b819846a1cd90964da893f4f8d3f3
Stadnyk Alexey <doctorstal@gmail.com>
2017-10-30 00:11:22 +0200

    Step two. Refactorings

--- js/string.calculator.js
+++ js/string.calculator.js
@@ -1,12 +1,5 @@
 function add(numbers) {
-    return numbers.length == 0 ?
-        0 :
-        addNumbers(numbers);
-}
-
-function addNumbers(numbers) {
-    var arr = numbers.split(',');
-    return arr.reduce(function(prev, curr) {
-        return prev + parseInt(curr);
+    return numbers.split(',').reduce(function(prev, curr) {
+        return prev + parseInt(curr || 0);
     }, 0);
 }
