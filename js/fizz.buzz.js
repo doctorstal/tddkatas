0814f22c95ca1d7e928b362920ab0b9cc94d4a24
Stadnyk Alexey <doctorstal@gmail.com>
2017-10-30 22:21:06 +0200

    String calculator kata start over

--- js/fizz.buzz.js
+++ js/fizz.buzz.js
@@ -1,13 +1,13 @@
 function fizzBuzz(number) {
     checkArguments();
-    return fizzAndOrBuzz() || number.toString();
+    return getFizzBuzzString() || number.toString();
 
     function checkArguments() {
         if (number <= 0)
             throw new Error('Numbers less than zero is not supported.');
     }
 
-    function fizzAndOrBuzz() {
+    function getFizzBuzzString() {
         var res = '';
         if (number % 3 == 0) res += 'Fizz';
         if (number % 5 == 0) res += 'Buzz';
