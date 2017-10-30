44f509d254c4243a2202b932c346826a772e86e5
Stadnyk Alexey <doctorstal@gmail.com>
2017-10-30 21:42:58 +0200

    =FizzBuzz first step

---
+++ js/fixx.buzz.spec.js
@@ -0,0 +1,8 @@
+describe("FizzBuzz", function() {
+    it("Should return numbers for non dividible by 3 and 5", function() {
+        expect(fizzBuzz(1)).toBe('1');
+        expect(fizzBuzz(2)).toBe('2');
+    })
+
+
+})
