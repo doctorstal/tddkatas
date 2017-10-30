e304233edb7f55ac4c9d91585dbe73f1341eeeee
Stadnyk Alexey <doctorstal@gmail.com>
2017-10-30 22:04:07 +0200

    =FizzBuzz refactored

--- js/fizz.buzz.js
+++ js/fizz.buzz.js
@@ -1,7 +1,16 @@
 function fizzBuzz(number) {
-    if (number <= 0) throw new Error('Numbers less than zero is not supported.');
-    var res = ''
-    if (number % 3 == 0) res += 'Fizz';
-    if (number % 5 == 0) res += 'Buzz';
-    return res || number.toString();
+    checkArguments();
+    return fizzAndOrBuzz() || number.toString();
+
+    function checkArguments() {
+        if (number <= 0)
+            throw new Error('Numbers less than zero is not supported.');
+    }
+
+    function fizzAndOrBuzz() {
+        var res = '';
+        if (number % 3 == 0) res += 'Fizz';
+        if (number % 5 == 0) res += 'Buzz';
+        return res;
+    }
 }
