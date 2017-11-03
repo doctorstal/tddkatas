68a1442611113ef30d710e3d9846bb9d8c8d20fe
Stadnyk Alexey <doctorstal@gmail.com>
2017-11-03 22:29:32 +0200

    FizzBuzz oneliner refactor

--- js/fizz.buzz.js
+++ js/fizz.buzz.js
@@ -1,7 +1,5 @@
 function fizzBuzz(num) {
-    var res = '';
-    if (num % 3 == 0) res += 'Fizz';
-    if (num % 5 == 0) res += 'Buzz';
-    if (res.length == 0) res = String(num);
-    return res;
+    return ((num % 3 ? '' : 'Fizz') +
+            (num % 5 ? '' : 'Buzz')) ||
+        String(num);
 }
