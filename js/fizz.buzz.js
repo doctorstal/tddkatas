fa50a3a7cb174b686c26a9c8095fc4a66a43d6f6
alexeyst <Alexey.Stadnik@playtech.com>
2017-12-16 09:17:11 +0200

    FizzBuzz kata refactor.

--- js/fizz.buzz.js
+++ js/fizz.buzz.js
@@ -1,8 +1,5 @@
 function fizzBuzz(number) {
-    return number % 15 == 0 
-    ? 'FizzBuzz' :
-    number % 3 == 0 
-    ? 'Fizz' :
-    number % 5 == 0 ? 'Buzz' :
-     number.toString();
+    return (number % 3 == 0 ? 'Fizz' : '')
+        + (number % 5 == 0 ? 'Buzz' : '')
+        || number.toString();
 }
